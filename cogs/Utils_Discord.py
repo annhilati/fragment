dd0388a7047db43300d21520b9281c600395459f
Annhilati <antbe.artis@web.de>
2024-04-24 13:11:51 +0200

    Sync

--- cogs/Utils_Discord.py
+++ cogs/Utils_Discord.py
@@ -13,16 +13,15 @@ class Utils_Discord(commands.Cog):
     def __init__(self, client: commands.Bot) -> None:
         self.client = client
 
-    ### Benutzerdetails Kontextpunkt
+    ######### Benutzerdetails Kontextpunkt
+
         self.ctx_menu = app_commands.ContextMenu(
             name='Benutzerdetails anzeigen',
             callback=self.user_details,
         )
         self.client.tree.add_command(self.ctx_menu)
-
     async def cog_unload(self) -> None:
         self.client.tree.remove_command(self.ctx_menu.name, type=self.ctx_menu.type)
-
     @app_commands.guilds()
     async def user_details(self, interaction: discord.Interaction, member: discord.Member) -> None:
         await interaction.response.send_message(f'{member} joined at {discord.utils.format_dt(member.joined_at)}')
--- main.py
+++ main.py
@@ -6,25 +6,52 @@ from dotenv import load_dotenv
 from itertools import cycle
 import os
 
+#-------------------------------------------------#
+#             Funktionsdefinitionen               #
+#                     log()                       #
+#-------------------------------------------------#
+
 def log(text):
     return print("[" + datetime.datetime.now().strftime("%H:%M:%S") + "] " + text)
 
+#-------------------------------------------------#
+#               Initialisierung                   #
+#                 Botinstanz                      #
+#-------------------------------------------------#
+
 client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
-@client.command()
-async def tree_sync(ctx):
-    await client.tree.sync()
-    log("[Conn] App Commands have been synchronized by someone")
+#-------------------------------------------------#
+#                Error-Handling                   #
+#-------------------------------------------------#
 
-############ Laden der Cogs
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("Fehlende Berechtigung")
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Fehlende Argumente")
+
+
+#-------------------------------------------------#
+#              App-Synchronisierung               #
+#-------------------------------------------------#
+
+@client.command()
+async def tree_sync(ctx, arg=None):
+    if arg == client.user.id:
+        await client.tree.sync()
+        log("[Conn] App Commands have been synchronized by someone")
+
+#-------------------------------------------------#
+#                 Hauptprogramm                   #
+#-------------------------------------------------#
 
 async def load():
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
             await client.load_extension(f"cogs.{filename[:-3]}")
             log(f"[Cogs] cogs/{filename} is loaded")
-
-############ Ausführung
 
 async def main():
     load_dotenv() # Läd die Umgebungsvariabeln
