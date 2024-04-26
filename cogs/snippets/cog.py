7f5dbb683285e0e1aa8cb59cef0fe6e3f5dac02e
Annhilati <antbe.artis@web.de>
2024-04-26 11:23:07 +0200

    Sync

---
+++ cogs/snippets/cog.py
@@ -0,0 +1,27 @@
+import datetime
+import discord
+from discord.ext import commands
+
+#-------------------------------------------------#
+#             Funktionsdefinitionen               #
+#                     log()                       #
+#-------------------------------------------------#
+
+def log(text):
+    return print("[" + datetime.datetime.now().strftime("%H:%M:%S") + "] " + text)
+
+#-------------------------------------------------#
+#                cog-Deklaration                  #
+#-------------------------------------------------#
+
+async def setup(client):
+    await client.add_cog(MyCog(client))
+
+
+class MyCog(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        log(f"[COGS] MyCog is ready")
