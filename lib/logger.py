36a74956d1783a771a7c076120101fa710e0de54
Annhilati <antbe.artis@web.de>
2024-05-19 11:52:15 +0200

    Sync

---
+++ lib/logger.py
@@ -0,0 +1,9 @@
+import datetime
+
+#-------------------------------------------------#
+#             Funktionsdefinitionen               #
+#                     log()                       #
+#-------------------------------------------------#
+
+def log(text):
+    return print("[" + datetime.datetime.now().strftime("%H:%M:%S") + "] " + text)
